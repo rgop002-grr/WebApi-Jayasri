01333a7e27dca69c704e41c5423caec35ed089ed
Jayasri Ethini <ethinijayasri1207@gmail.com>
2025-12-10 19:08:50 +0530

    Implemented Routing both Attribute and Conventional.

---
+++ WebApi-Jayasri/Controllers/RoutProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Jayasri.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutProductsController : ControllerBase
+    {
+       [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Ok("All products");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"Product {id}");
+        }
+    }
+}
+
